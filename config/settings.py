02e7bd63311961b0006a70c2ed800fc62220c11f
ChanHaeng Lee <2chanhaeng@gmail.com>
2022-10-13 08:16:42 +0900

    Set SECRET_KEY as env var
    SECRET_KEY를 가상환경 환경변수로 숨겼습니다. 임시 프로젝트이기에 따로 변경하지는 않았습니다.

--- config/settings.py
+++ config/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+from os import environ
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-hu@h8tm2=m&xz5s5ni--@%(10!qqbhmx!wb-tnq908@yjyqbcc'
+SECRET_KEY = environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
