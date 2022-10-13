8f834298a8869deda7f98d6fd01ac564c1ab2033
ChanHaeng Lee <2chanhaeng@gmail.com>
2022-10-14 08:23:08 +0900

    Create Company, User Form
    회사와 사용자 폼 제작했습니다. 아직은 이름만 지정할 수 있고 ID 및 시간은 서버에서 자동으로 추가를 해줄 것이기 때문에 따로 추가하지 않았습니다.

---
+++ wanted/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+from wanted.models import Company, User
+
+
+class CompanyForm(forms.ModelForm):
+    class Meta:
+        model = Company
+        fields = ['name']
+
+
+class UserForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['name']
