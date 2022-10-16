f3e2601773a9936a0f2c1c197aabadcc3c379315
ChanHaeng Lee <2chanhaeng@gmail.com>
2022-10-16 16:52:23 +0900

    Create Serializer, ViewSet of Notice, Application
    Notice과 Application 모델의 Serializer와 ViewSet을 제작했습니다. 또한 Company, User 모델에서도 외래키로 사용된 열을 참조할 수 있도록 Serializer와 ViewSet을 수정했습니다.

--- wanted/api.py
+++ wanted/api.py
@@ -1,11 +1,38 @@
+from email.mime import application
 from rest_framework import serializers, viewsets
-from .models import Company, User
+from .models import Company, User, Notice, Application
+
+
+class ApplicationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Application
+        fields = ("notice", "user")
+
+
+class ApplicationViewSet(viewsets.ModelViewSet):
+    queryset = Application.objects.all()
+    serializer_class = ApplicationSerializer
+
+
+class NoticeSerializer(serializers.ModelSerializer):
+    application = ApplicationSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Notice
+        fields = ("name", "company", "application")
+
+
+class NoticeViewSet(viewsets.ModelViewSet):
+    queryset = Notice.objects.all()
+    serializer_class = NoticeSerializer
 
 
 class CompanySerializer(serializers.ModelSerializer):
+    notice = NoticeSerializer(many=True, read_only=True)
+
     class Meta:
         model = Company
-        fields = ("name",)
+        fields = ("name", "notice")
 
 
 class CompanyViewSet(viewsets.ModelViewSet):
@@ -14,9 +41,11 @@ class CompanyViewSet(viewsets.ModelViewSet):
 
 
 class UserSerializer(serializers.ModelSerializer):
+    application = ApplicationSerializer(many=True, read_only=True)
+
     class Meta:
         model = User
-        fields = ("name",)
+        fields = ("name", "application")
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
