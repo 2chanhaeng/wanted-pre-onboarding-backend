91a977433959c7ebb0025cbf0fb7f1c6fa690449
ChanHaeng Lee <2chanhaeng@gmail.com>
2022-10-16 14:04:24 +0900

    Make date fill auto
    등록 시간을 자동으로 채워넣을 수 있도록 만들었습니다.

--- wanted/models.py
+++ wanted/models.py
@@ -3,21 +3,21 @@ from django.db import models
 
 class Company(models.Model):
     name = models.CharField(max_length=256)
-    date = models.DateTimeField()
+    date = models.DateTimeField(auto_now_add=True)
 
 
 class User(models.Model):
     name = models.CharField(max_length=256)
-    date = models.DateTimeField()
+    date = models.DateTimeField(auto_now_add=True)
 
 
 class Notice(models.Model):
     name = models.CharField(max_length=256)
-    date = models.DateTimeField()
+    date = models.DateTimeField(auto_now_add=True)
     company = models.ForeignKey(Company, on_delete=models.CASCADE)
 
 
 class Application(models.Model):
-    date = models.DateTimeField()
+    date = models.DateTimeField(auto_now_add=True)
     notice = models.ForeignKey(Company, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
