57d74d71be62839feb003d857f0a728beb3c3359
ChanHaeng Lee <2chanhaeng@gmail.com>
2022-10-16 16:48:43 +0900

    Create ID and related name of foreign key of model
    ID를 명시했으며 Notice, Application 모델에서 다른 모델의 외래키를 사용하기 위해 related_name을 지정했습니다.

--- wanted/models.py
+++ wanted/models.py
@@ -2,22 +2,26 @@ from django.db import models
 
 
 class Company(models.Model):
+    id = models.BigAutoField(primary_key=True)
     name = models.CharField(max_length=256)
     date = models.DateTimeField(auto_now_add=True)
 
 
 class User(models.Model):
+    id = models.BigAutoField(primary_key=True)
     name = models.CharField(max_length=256)
     date = models.DateTimeField(auto_now_add=True)
 
 
 class Notice(models.Model):
+    id = models.BigAutoField(primary_key=True)
     name = models.CharField(max_length=256)
     date = models.DateTimeField(auto_now_add=True)
-    company = models.ForeignKey(Company, on_delete=models.CASCADE)
+    company = models.ForeignKey(Company, on_delete=models.CASCADE, related_name="notice")
 
 
 class Application(models.Model):
+    id = models.BigAutoField(primary_key=True)
     date = models.DateTimeField(auto_now_add=True)
-    notice = models.ForeignKey(Company, on_delete=models.CASCADE)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    notice = models.ForeignKey(Company, on_delete=models.CASCADE, related_name="application")
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="application")
