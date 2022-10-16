baffda39e01889202d3926fd8418c8b5045ebf11
ChanHaeng Lee <2chanhaeng@gmail.com>
2022-10-16 16:53:34 +0900

    Register Notice and Application to router
    라우터에 Notice와 Application을 등록했습니다.

--- wanted/urls.py
+++ wanted/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 from django.conf.urls import include
 from rest_framework import routers
-from .api import CompanyViewSet, UserViewSet
+from .api import CompanyViewSet, UserViewSet, NoticeViewSet, ApplicationViewSet
 
 router = routers.DefaultRouter()
 router.register('company', CompanyViewSet)
 router.register('user', UserViewSet)
+router.register('notice', NoticeViewSet)
+router.register('application', ApplicationViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
