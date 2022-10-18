7bd999b9246bdc133f1a43a35d21a1aef30d7843
ChanHaeng Lee <2chanhaeng@gmail.com>
2022-10-18 19:41:09 +0900

    Delete related_name of ForeignKey
    related_name을 지정했으나 오히려 헷갈릴 가능성이 있다고 판단하여 삭제하고 기본적으로 지정되는 notice_set, application_set을 사용하도록 변경하였습니다.

--- wanted/models.py
+++ wanted/models.py
@@ -17,11 +17,11 @@ class Notice(models.Model):
     id = models.BigAutoField(primary_key=True)
     name = models.CharField(max_length=256)
     date = models.DateTimeField(auto_now_add=True)
-    company = models.ForeignKey(Company, on_delete=models.CASCADE, related_name="notice")
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
 
 
 class Application(models.Model):
     id = models.BigAutoField(primary_key=True)
     date = models.DateTimeField(auto_now_add=True)
-    notice = models.ForeignKey(Notice, on_delete=models.CASCADE, related_name="application")
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="application")
+    notice = models.ForeignKey(Notice, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
